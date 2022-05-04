f81aebf4013a0e259f8f68e795fc195eac236f4e
Zaur <len1in@yandex.ru>
2022-05-04 18:31:28 +0300

    Subscription implementation via the Soketi socket server

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,10 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
+                'host' => env('PUSHER_HOST', '127.0.0.1'),
+                'port' => env('PUSHER_PORT', 6001),
+                'scheme' => env('PUSHER_SCHEME', 'http'),
+                'useTLS' => env('PUSHER_SCHEME') === 'https',
             ],
         ],
 
